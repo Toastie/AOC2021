dc12017a3b0e6ebaa91e5a94f849d9edf8c63a72
Simon Kopischke <32514734+Toastie@users.noreply.github.com>
2021-12-03 14:38:54 +0100

    problem 2 cleaned up

--- day3/ptoblem2.py
+++ day3/ptoblem2.py
@@ -17,20 +17,14 @@ def mostCommonBit(data:list,pos:int):
         return 0
 
 oxygen = data
-
-for i in range(len(data[0])):
-    if len(oxygen) == 1:
-        break
-    bit = mostCommonBit(oxygen,i)
-    oxygen = [value for value in oxygen if int(value[i]) == bit]
-
 co2 = data
 
 for i in range(len(data[0])):
-    if len(co2) == 1:
-        break
-    bit = mostCommonBit(co2,i)
-    co2 = [value for value in co2 if int(value[i]) != bit]
-
+    if len(oxygen) != 1:
+        bit = mostCommonBit(oxygen,i)
+        oxygen = [value for value in oxygen if int(value[i]) == bit]
+    if len(co2) != 1:
+        bit = mostCommonBit(co2,i)
+        o2 = [value for value in co2 if int(value[i]) != bit]
 
 print(f"Oxygen: {oxygen}, CO2: {co2}, Multiplied: {int(co2[0],2) * int(oxygen[0],2)}")
